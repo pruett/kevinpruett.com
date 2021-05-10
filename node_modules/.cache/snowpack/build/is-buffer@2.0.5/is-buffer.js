524178155f28e6ed8010a8fe97557b59f0e2890b
Kevin Pruett <pruett.kevin@gmail.com>
2021-05-10 19:35:20 -0400

    use astro

---
+++ node_modules/.cache/snowpack/build/is-buffer@2.0.5/is-buffer.js
@@ -0,0 +1,13 @@
+/*!
+ * Determine if an object is a Buffer
+ *
+ * @author   Feross Aboukhadijeh <https://feross.org>
+ * @license  MIT
+ */
+
+var isBuffer = function isBuffer (obj) {
+  return obj != null && obj.constructor != null &&
+    typeof obj.constructor.isBuffer === 'function' && obj.constructor.isBuffer(obj)
+};
+
+export default isBuffer;
---
+++ node_modules/@snowpack/plugin-vue/plugin.js
@@ -0,0 +1,159 @@
+const fs = require('fs');
+const path = require('path');
+const hashsum = require('hash-sum');
+const compiler = require('@vue/compiler-sfc');
+const scriptCompilers = require('./src/script-compilers');
+
+/** Friendly error display */
+function displayError({contents, filePath, error}) {
+  const pad = (number, pad) => `${Array.from(new Array(pad + 1)).join(' ')}${number}`;
+
+  let output = [`${error.toString()}`, `[${filePath}]`];
+  if (error.loc) {
+    output[1] += ` Line ${error.loc.start.line}, Column ${error.loc.start.column}`;
+    const lineNo = (number) =>
+      ' ' +
+      pad(number, (error.loc.end.line + 1).toString().length - number.toString().length) +
+      ' | ';
+    output.push('');
+    const allLines = ['', ...contents.split('\n')];
+    let currentLine = error.loc.start.line;
+    output.push(lineNo(currentLine - 1) + allLines[currentLine - 1]);
+    while (currentLine <= error.loc.end.line) {
+      output.push(lineNo(currentLine) + allLines[currentLine]);
+      currentLine++;
+    }
+    output.push(
+      Array.from(new Array(error.loc.start.column + lineNo(currentLine - 1).length)).join(' ') +
+        '^',
+    );
+    output.push(lineNo(currentLine) + allLines[currentLine]);
+  }
+  return output.join('\n');
+}
+
+module.exports = function plugin(snowpackConfig) {
+  return {
+    name: '@snowpack/plugin-vue',
+    resolve: {
+      input: ['.vue'],
+      output: ['.js', '.css'],
+    },
+    async load({filePath, isSSR}) {
+      const {sourcemap, sourceMaps} = snowpackConfig.buildOptions;
+
+      const id = hashsum(filePath);
+      const contents = fs.readFileSync(filePath, 'utf-8');
+      const {descriptor, errors} = compiler.parse(contents, {filename: filePath});
+
+      // display errors
+      if (errors && errors.length > 0) {
+        throw new Error(displayError({error: errors[0], contents, filePath}));
+      }
+
+      const output = {
+        '.js': {code: '', map: ''},
+        '.css': {code: '', map: ''},
+      };
+
+      if (descriptor.script) {
+        const scriptLang = descriptor.script.lang;
+        let scriptContent = descriptor.script.content;
+        if (['jsx', 'ts', 'tsx'].includes(scriptLang)) {
+          scriptContent = scriptCompilers.esbuildCompile(scriptContent, scriptLang);
+        }
+        if (['js', 'ts'].includes(scriptLang) || !scriptLang) {
+          scriptContent = scriptContent.replace(`export default`, 'const defaultExport =');
+        }
+        output['.js'].code += scriptContent;
+      } else {
+        output['.js'].code += `const defaultExport = {};\n`;
+      }
+
+      let hasCssModules = false;
+      const cssModules = {};
+
+      await Promise.all(
+        descriptor.styles.map(async (stylePart) => {
+          // note: compileStyleAsync is required for SSR + CSS Modules
+          const css = await compiler.compileStyleAsync({
+            filename: path.relative(snowpackConfig.root || process.cwd(), filePath),
+            source: stylePart.content,
+            id: `data-v-${id}`,
+            scoped: stylePart.scoped != null,
+            modules: stylePart.module != null,
+            ssr: isSSR,
+            preprocessLang: stylePart.lang,
+            // preprocessCustomRequire: (id: string) => require(resolve(root, id))
+            // TODO load postcss config if present
+          });
+
+          // gather CSS Module names
+          if (stylePart.module) {
+            hasCssModules = true;
+            for (const [k, v] of Object.entries(css.modules)) {
+              if (cssModules[k]) console.warn(`CSS Module name reused: ${k}`);
+              cssModules[k] = v;
+            }
+          }
+
+          if (css.errors && css.errors.length > 0) {
+            console.error(JSON.stringify(css.errors));
+          }
+          output['.css'].code += css.code;
+          if ((sourcemap || sourceMaps) && css.map) output['.css'].map += JSON.stringify(css.map);
+        }),
+      );
+
+      if (descriptor.template) {
+        const js = compiler.compileTemplate({
+          id,
+          filename: path.relative(snowpackConfig.root || process.cwd(), filePath),
+          source: descriptor.template.content,
+          ssr: isSSR,
+          ssrCssVars: [],
+          preprocessLang: descriptor.template.lang,
+          compilerOptions: {
+            scopeId: descriptor.styles.some((s) => s.scoped) ? `data-v-${id}` : null,
+          },
+        });
+        if (js.errors && js.errors.length > 0) {
+          console.error(JSON.stringify(js.errors));
+        }
+
+        const renderFn = isSSR ? `ssrRender` : `render`;
+
+        if (output['.js'].code) output['.js'].code += '\n';
+        output['.js'].code += `${js.code.replace(/;?$/, ';')}`; // add trailing semicolon if missing (helps with some SSR cases)
+        output['.js'].code += `\n\ndefaultExport.${renderFn} = ${renderFn};`;
+
+        // inject CSS Module styles, if needed
+        if (hasCssModules) {
+          // Note: this injection code is inspired by the official vue-loader for webpack and plays nicely with Vue.
+          // But adjustments were needed to work with Snowpack.
+          // See https://github.com/vuejs/vue-loader/blob/master/lib/codegen/styleInjection.js#L51
+          const styleInjectionCode = `  const cssModules = ${JSON.stringify(cssModules)};
+  Object.defineProperty(_ctx, '$style', {
+    configurable: true,
+    get: function() {
+      return cssModules;
+    }
+  })\n`;
+          output['.js'].code = output['.js'].code.replace(
+            new RegExp(`(function ${renderFn}\\([^\n]+)`),
+            `$1\n${styleInjectionCode}`,
+          );
+        }
+        output['.js'].code += `\n\nexport default defaultExport;`;
+
+        if ((sourcemap || sourceMaps) && js.map) output['.js'].map += JSON.stringify(js.map);
+      }
+
+      // clean up
+      if (!output['.js'].code) delete output['.js'];
+      if (!output['.css'].code) delete output['.css'];
+
+      return output;
+    },
+  };
+};
