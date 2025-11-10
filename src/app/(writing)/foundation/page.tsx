6d9f9abf5f6d828fe878c813823faa58804396e6
Kevin Pruett <pruett.kevin@gmail.com>
2025-11-10 15:39:07 -0500

    post

---
+++ src/app/(writing)/foundation/page.tsx
@@ -0,0 +1,33 @@
+import Foundation from "@/app/md/foundation.mdx";
+import {
+  Post,
+  PostAuthor,
+  PostContent,
+  PostDate,
+  PostMeta,
+  PostTitle,
+} from "@/components/post";
+
+export default function Page() {
+  return (
+    <Post className="flex flex-col gap-15">
+      <PostMeta className="flex flex-col gap-3 animate-fade-in [--stagger:1]">
+        <PostTitle className="text-xl font-medium">AI Foundation</PostTitle>
+        <div>
+          <PostAuthor className="text-base font-normal">
+            Kevin Pruett
+          </PostAuthor>
+          <PostDate className="text-base font-light">
+            November, 10, 2025
+          </PostDate>
+        </div>
+      </PostMeta>
+
+      <main className="flex flex-col gap-2">
+        <PostContent className="animate-fade-in [--stagger:3] font-inter">
+          <Foundation />
+        </PostContent>
+      </main>
+    </Post>
+  );
+}
--- src/app/_sections/writing.tsx
+++ src/app/_sections/writing.tsx
@@ -4,15 +4,27 @@ export function Writing() {
   return (
     <section
       id="writing"
-      className="animate-fade-in [--stagger:8] flex flex-col gap-2"
+      className="animate-fade-in [--stagger:8] flex flex-col gap-3"
     >
       <h2 className="font-medium text-lg">Writing</h2>
 
       <div className="flex flex-col gap-3">
         <HoverLinkCard
+          href="/foundation"
+          title="AI Foundation"
+          description="Software engineering in the AI era – from first principles. Part 1. An overview of some core concepts from: LLMs, tokens, context, agents, and tools."
+          badge={
+            <span className="text-xs text-muted-foreground font-mono uppercase">
+              November 2025
+            </span>
+          }
+        />
+      </div>
+      <div className="flex flex-col gap-3">
+        <HoverLinkCard
           href="/interfaces"
           title="Interfaces"
-          description="A quick explorationg into interfaces both old and new – from MCP to CLI"
+          description="A quick exploration into interfaces both old and new – from MCP to CLI"
           badge={
             <span className="text-xs text-muted-foreground font-mono uppercase">
               November 2025
